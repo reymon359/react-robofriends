ad29ca9133c6469084bbf9f162c86b4acb89edc4
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-02-24 07:27:26 +0100

    Create card component and pass robots info

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import 'tachyons';
 import * as serviceWorker from './serviceWorker';
+import { robots } from './robots';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <Card id={robots[0]} name={robots[0].name} email={robots[0].email}/>
+    </div>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
