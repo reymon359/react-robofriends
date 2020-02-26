3a0a144c5f9c0576ceb69938101b2f81d41629ee
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-02-26 06:21:11 +0100

    Publish app

---
+++ precache-manifest.e6fef7528dc671eff334bbcbb3f908cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84e233e714f53697959c",
+    "url": "/react-robofriends/static/css/main.529acb2f.chunk.css"
+  },
+  {
+    "revision": "84e233e714f53697959c",
+    "url": "/react-robofriends/static/js/main.2b4e04b0.chunk.js"
+  },
+  {
+    "revision": "238297068e2e665c321e",
+    "url": "/react-robofriends/static/js/runtime~main.41660cae.js"
+  },
+  {
+    "revision": "d1d6a8b2b8e3ac2e15db",
+    "url": "/react-robofriends/static/css/2.a6a9f571.chunk.css"
+  },
+  {
+    "revision": "d1d6a8b2b8e3ac2e15db",
+    "url": "/react-robofriends/static/js/2.2b47b015.chunk.js"
+  },
+  {
+    "revision": "bee9144f3ae7eb34938e846f128c67e4",
+    "url": "/react-robofriends/static/media/Street-Robot-Inline.bee9144f.woff"
+  },
+  {
+    "revision": "a904f014fa8d902a94f65a27cf07c0b0",
+    "url": "/react-robofriends/index.html"
+  }
+];
