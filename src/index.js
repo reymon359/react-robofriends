c35e48a0637df1cafd66d50fa7976ebef873d6b9
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-02-26 07:56:56 +0100

    Install redux-thunk and create constants

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 import { searchRobots } from './reducers';
 
-const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const logger = createLogger(); // MIddleware for logs
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
     <Provider store={store}>
