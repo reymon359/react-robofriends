0d0491745fdf660a5c3d2b1a513310cc17132260
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-02-29 14:30:18 +0100

    Create header component

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+// In most cases, instead of writing shouldComponentUpdate()
+// by hand, you can inherit from React.PureComponent. It is
+// equivalent to implementing shouldComponentUpdate() with a
+// shallow comparison of current and previous props and state.
+
+class Header extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return false
+  }
+  render() {
+    return (
+      <div>
+        <h1 className='f1'>RoboFriends</h1>
+      </div>
+    );
+  }
+};
+
+export default Header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CardList from '../components/CardList';
+import Header from '../components/Header';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
@@ -30,24 +31,24 @@ class App extends Component {
     }
 
     render() {
-        const { searchField, onSearchchange, robots, isPending  } = this.props;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
 
-        return isPending ? (
-            <h1 className="f1 tc">Loading...</h1>
-        ) : (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={onSearchchange} />
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots={filteredRobots} />
-                    </ErrorBoundary>
-                </Scroll>
+        return (
+            <div className='tc'>
+             <Header/>
+              <SearchBox searchChange={onSearchChange}/>
+              <Scroll>
+                { isPending ? <h1>Loading</h1> :
+                  <ErrorBoundary>
+                    <CardList robots={filteredRobots} />
+                  </ErrorBoundary>
+                }
+              </Scroll>
             </div>
-        );
+          );
     }
 }
 
